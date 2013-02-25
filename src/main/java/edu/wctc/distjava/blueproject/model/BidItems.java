5efd961cad1bfa6f81c1b5000c6b2b76dea49129
Patrick Urban <production@localhost>
2013-02-25 09:15:23 -0600

    Added more faux database items to test paginator feature of the datatable. Currently data lost when switching pages.

--- src/main/java/edu/wctc/distjava/blueproject/model/BidItems.java
+++ src/main/java/edu/wctc/distjava/blueproject/model/BidItems.java
@@ -22,7 +22,8 @@ public class BidItems {
         List<AuctionItem> items = new ArrayList<AuctionItem>();
         
         items.add(new AuctionItem("1", "Electronics", "Blu-Ray Player", 
-                "Samsung Blu-Ray player with Internet capabilities", "50.00", "3/15/2013"));
+                "Samsung Blu-Ray player with Internet capabilities", "50.00", 
+                "3/15/2013"));
         items.add(new AuctionItem("2", "Electronics", "iPod Touch", 
                 "Apple iPod Touch 32GB", "175.00", "3/15/2013"));
         items.add(new AuctionItem("3", "Electronics", "USB Drive", 
@@ -39,6 +40,22 @@ public class BidItems {
         items.add(new AuctionItem("8", "Dining", "Harry Joe's Diner", 
                 "Two $25.00 Gift certificates to eat at Harry Joe's Diner", 
                 "15.00", "3/15/2013"));
+        items.add(new AuctionItem("9", "Electronics", "X-Box 360", 
+                "X-Box 360 halo 4 Edition from GameStop", "100.00", "3/15/2013"));
+        items.add(new AuctionItem("10", "Electronics", "Nintendo 3DS XL", 
+                "Nintendo 3DS XL and 1 game from GameStop", "150.00", "3/15/2013"));
+        items.add(new AuctionItem("11", "Dining", "$50 Gift Certificates Servlet Slophouse", 
+                "Two $25.00 Gift certificates for use at the Servlet Slophouse", 
+                "25.00", "3/15/2013"));
+        items.add(new AuctionItem("12", "Retail", "$50 Best Buy Gift Card", 
+                "$50 Best Buy Gift Card", "20.00", "3/15/2013"));
+        items.add(new AuctionItem("13", "Electronics", "16BG USB Flash Drive", 
+                "PNY 16BG USB Flash Drive", "5.00", "3/15/2013"));
+        items.add(new AuctionItem("14", "Electronics", "32BG USB Flash Drive", 
+                "PNY 32BG USB Flash Drive", "12.00", "3/15/2013"));
+        items.add(new AuctionItem("15", "Electronics", "E-FLITE BLADE CX3 Police Helicopter", 
+                "A blade cx3 copter in good used condition ...Comes with "
+                + "everything needed to fly.", "35.00", "3/15/2013"));
         
         return items;
     }
