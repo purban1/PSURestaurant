f6a952e4e2d3c6a5d42e2dec2267d6bc1b2d056c
Patrick Urban <production@localhost>
2013-02-28 17:19:07 -0600

    Setup for DB access

--- src/main/java/edu/wctc/distjava/blueproject/controller/SampleBean.java
+++ src/main/java/edu/wctc/distjava/blueproject/controller/SampleBean.java
@@ -1,20 +1,25 @@
 package edu.wctc.distjava.blueproject.controller;
 
+import edu.wctc.distjava.blueproject.model.Employee;
+import edu.wctc.distjava.blueproject.model.EmployeeEAO;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
  * A sample JSF Managed Bean
  * @author  jlombardo
  * @version 1.00
  */
-@ManagedBean(name = "sample")
+@Named(value = "sample")
 @ViewScoped
 public class SampleBean implements Serializable {
     // Incremement this with each structural class change
     private static final long serialVersionUID = 1L;
-    
+    @Inject
+    private EmployeeEAO eao;
     private String message = "Hello From JSF Managed Bean";
 
     /**
@@ -31,5 +36,9 @@ public class SampleBean implements Serializable {
         this.message = message;
     }
     
+    public String getEmployeeById(String id){
+        Employee employee = eao.getEmployeeByID(new Integer(id));
+        return employee.toString();
+    }
     
 }
---
+++ src/main/java/edu/wctc/distjava/blueproject/model/EmployeeEAO.java
@@ -0,0 +1,38 @@
+package edu.wctc.distjava.blueproject.model;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+/**
+ *
+ * @author Patrick Urban
+ */
+
+@Named
+@Stateless
+public class EmployeeEAO {
+
+    @PersistenceContext(unitName = "blueprojectPU")
+    private EntityManager em;
+
+    public Employee getEmployeeByID(Object id) {
+        return getEntityManager().find(Employee.class, id);
+    }
+
+    /**
+     * Always use this in your code -- never reference the 'em' property because
+     * JPA EntityManagers are loaded on demand.
+     *     
+     * @return an on demand object
+     */
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+    
+    
+}
